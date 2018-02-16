db14139493fb4a312a5ccf2db19a1b3790cb6ee7
entrptaher@gmail.com <entrptaher@gmail.com>
2018-02-16 07:03:02 +0600

    Correct all tests

--- index.js
+++ index.js
@@ -1,45 +1,75 @@
 function cleanUpArray(arr) {
-    let newArr = [];
-    for (let [index, group] of arr.entries()) {
-        const prev = newArr[index - 1];
-        const newGroup = [];
-
-        for (let [innerIndex, elem] of group.entries()) {
-            if (elem && prev && !prev[innerIndex] && !group[0]) {
-                prev[innerIndex] = elem;
-                newGroup.push(0)
-            } else {
-                newGroup.push(elem)
-            }
+  let newArr = [];
+  for (let [index, group] of arr.entries()) {
+    const newGroup = [];
+    for (let [innerIndex, elem] of group.entries()) {
+      if (
+        !group[0] &&
+        elem &&
+        (newArr[index - 1] && !newArr[index - 1][innerIndex])
+      ) {
+        if (!newArr[index - 1][0] && !newArr[index - 2][innerIndex]) {
+          newArr[index - 2][innerIndex] = elem;
+        } else {
+          newArr[index - 1][innerIndex] = elem;
         }
-
-        if (!newGroup.every(elem => !elem)) {
-            newArr.push(newGroup);
-        }
+        newGroup.push(0);
+      } else {
+        newGroup.push(elem);
+      }
     }
+    newArr.push(newGroup);
+  }
 
-    // cleanup empty values from array
-    return newArr
+  // cleanup empty values from array
+  return newArr.filter(group => !group.every(elem => !elem));
 }
 
 const dataSet = [
-    { provided: cleanUpArray([[1, 0, 0, 1], [0, 1, 1, 1]]), expected: [[1, 1, 1, 1], [0, 0, 0, 1]] },
-    { provided: cleanUpArray([[1, 0, 0, 1], [0, 1, 0, 1]]), expected: [[1, 1, 0, 1], [0, 0, 0, 1]] },
-    { provided: cleanUpArray([[1, 0, 0], [1, 0, 1], [0, 1, 0], [0, 0, 1]]), expected: [[1, 0, 0], [1, 1, 1], [0, 0, 1]] },
-    { provided: cleanUpArray([[1, 0, 0], [0, 1, 0], [0, 0, 1]]), expected: [[1, 1, 1]] },
-    { provided: cleanUpArray([[1, 0, 0], [0, 1, 1], [1, 0, 1]]), expected: [[1, 1, 1], [1, 0, 1]] },
-    { provided: cleanUpArray([[1, 0, 0], [0, 0, 1], [0, 1, 1]]), expected: [[1, 1, 1], [0, 0, 1]] },
-    { provided: cleanUpArray([[1, 0, 0], [0, 0, 1], [0, 1, 1], [1, 0, 0]]), expected: [[1, 1, 1], [0, 0, 1], [1, 0, 0]] },
+  {
+    provided: cleanUpArray([[1, 0, 0, 1], [0, 1, 1, 1]]),
+    expected: [[1, 1, 1, 1], [0, 0, 0, 1]]
+  },
+  {
+    provided: cleanUpArray([[1, 0, 0, 1], [0, 1, 0, 1]]),
+    expected: [[1, 1, 0, 1], [0, 0, 0, 1]]
+  },
+  {
+    provided: cleanUpArray([[1, 0, 0], [1, 0, 1], [0, 1, 0], [0, 0, 1]]),
+    expected: [[1, 0, 0], [1, 1, 1], [0, 0, 1]]
+  },
+  {
+    provided: cleanUpArray([
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1],
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1]
+    ]),
+    expected: [[1, 1, 1], [1, 1, 1]]
+  },
+  {
+    provided: cleanUpArray([[1, 0, 0], [0, 1, 1], [1, 0, 1]]),
+    expected: [[1, 1, 1], [1, 0, 1]]
+  },
+  {
+    provided: cleanUpArray([[1, 0, 0], [0, 0, 1], [0, 1, 1]]),
+    expected: [[1, 1, 1], [0, 0, 1]]
+  },
+  {
+    provided: cleanUpArray([[1, 0, 0], [0, 0, 1], [0, 1, 1], [1, 0, 0]]),
+    expected: [[1, 1, 1], [0, 0, 1], [1, 0, 0]]
+  }
 ];
 
-var assert = require('assert');
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        dataSet.forEach(elem => {
-            it('should match', function () {
-                // assert.equal([1, 2, 3].indexOf(4), -1);
-                assert.deepEqual(elem.provided, elem.expected)
-            })
-        });
+var assert = require("assert");
+describe("Array", function() {
+  describe("#indexOf()", function() {
+    dataSet.forEach(elem => {
+      it("should match", function() {
+        assert.deepEqual(elem.provided, elem.expected);
+      });
     });
+  });
 });
